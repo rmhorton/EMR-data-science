a85f42c3c177b8a58df46a78dee574ce1c611f01
Robert Horton <rhorton@microsoft.com>
2022-11-10 13:00:44 -0800

    mount storage container with SAS token

--- 0_Load_data.py
+++ 0_Load_data.py
@@ -61,44 +61,45 @@ for file_info in dbutils.fs.ls('/FileStore/emr_sample/csv'):
 
 # MAGIC %md
 # MAGIC 
-# MAGIC # Load big dataset from blob storage
+# MAGIC # Load big dataset from storage container
 
 # COMMAND ----------
 
 # DBTITLE 1,Enter your data connection information here
-secrets = {'storage_account_name':'PUT_YOUR_BLOB_STORAGE_ACCOUNT_NAME_HERE',
-           'container_name':'PUT_YOUR_BLOB_CONTAINER_NAME_HERE',
-           'storage_account_key':'PUT_YOUR_BLOB_STORAGE_ACCOUNT_KEY_HERE'}
+secrets = {'storage_account_name':'syntheauploadsa',
+           'container_name':'syntheadata1',
+           'data_path': '/missouri/2021_07_11T17_42_12Z_parquet',
+           'sas_token':'PUT_YOUR_SAS_TOKEN_HERE'}
 
 # COMMAND ----------
 
-if secrets['storage_account_name'] == 'PUT_YOUR_BLOB_STORAGE_ACCOUNT_NAME_HERE':
+if secrets['sas_token'] == 'PUT_YOUR_SAS_TOKEN_HERE':
     displayHTML('''<blink><font color="red"><h1>You need to enter your connection info in the 'secrets' dict!</h1></font></blink>''')
 
 # COMMAND ----------
 
 # DBTITLE 1,Mount storage container to DBFS
-CSV_FOLDER = "/missouri/2021_07_11T17_42_12Z"
+DATA_SOURCE = "wasbs://{container_name}@{storage_account_name}.blob.core.windows.net".format(**secrets)
+
+DATA_PATH = secrets['data_path']
 
 DB_NAME = 'missouri'
 
-DATA_SOURCE = "wasbs://{container_name}@{storage_account_name}.blob.core.windows.net".format(**secrets)
+MOUNT_POINT = f"/mnt/{DB_NAME}"
 
-EXTRA_CONFIGS = {"fs.azure.account.key.{storage_account_name}.blob.core.windows.net".format(**secrets): secrets['storage_account_key']}
+EXTRA_CONFIGS = {"fs.azure.sas.{container_name}.{storage_account_name}.blob.core.windows.net".format(**secrets): secrets['sas_token']}
 
 CURRENTLY_MOUNTED = {mount_info.mountPoint for mount_info in dbutils.fs.mounts()}
-
-
-if f"/mnt/{DB_NAME}" in CURRENTLY_MOUNTED: 
-  dbutils.fs.unmount(f"/mnt/{DB_NAME}")
+if MOUNT_POINT in CURRENTLY_MOUNTED: 
+  dbutils.fs.unmount(MOUNT_POINT)
 
 dbutils.fs.mount(
-    source = DATA_SOURCE + CSV_FOLDER,
-    mount_point = f"/mnt/{DB_NAME}",
+    source = DATA_SOURCE + DATA_PATH,
+    mount_point = MOUNT_POINT,
     extra_configs = EXTRA_CONFIGS
 )
 
-[f.name for f in dbutils.fs.ls(f"/mnt/{DB_NAME}")]
+[f.name for f in dbutils.fs.ls(MOUNT_POINT)]
 
 # COMMAND ----------
 
@@ -108,7 +109,14 @@ import re
 spark.sql(f"create database if not exists {DB_NAME}")
 spark.sql(f"use {DB_NAME}")
 
-for file_info in dbutils.fs.ls(f"/mnt/{DB_NAME}"):
-  table_name = re.sub('(.*)\\.csv$', '\\1', file_info.name).lower()
+for file_info in dbutils.fs.ls(MOUNT_POINT):
+  table_name = re.sub('(.*)\\.parquet/$', '\\1', file_info.name).lower()
   print(f"creating table '{DB_NAME}.{table_name}' from file {file_info.path}")
-  spark.read.options(header=True).csv(file_info.path).write.mode('overwrite').saveAsTable(table_name)
+  spark.read.parquet(file_info.path).write.mode('overwrite').saveAsTable(table_name)
+
+## If you change your mind:
+#   spark.sql(f"drop database {DB_NAME} cascade")
+
+# COMMAND ----------
+
+
